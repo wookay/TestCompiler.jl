51c71b92745a0a19acea379794814bb88d8d5cd5
WooKyoung Noh <wookay.noh@gmail.com>
2025-04-09 15:14:46 +0900

    test TestCompiler.PartitionKind

--- test/compiler/jl_partition_kind.jl
+++ test/compiler/jl_partition_kind.jl
@@ -2,7 +2,15 @@ using Jive
 @If VERSION >= v"1.13.0-DEV.151" module test_compiler_jl_partition_kind
 
 using Test
-using TestCompiler
+using TestCompiler # PartitionKind
+
+function sprint_plain(x)::String
+    sprint(io -> show(io, MIME"text/plain"(), x))
+end
+
+@test PartitionKind(0x9) == PartitionKind(Base.PARTITION_KIND_GUARD)
+@test sprint_plain(PartitionKind(Base.PARTITION_KIND_GUARD)) ==
+      """const PARTITION_KIND_GUARD = 0x9\n  Guard: The binding was looked at, but no global or import was resolved at\n  the time\n\n  ->restriction is NULL."""
 
 # from julia/test/rebinding.jl
 
@@ -11,8 +19,14 @@ using TestCompiler
 struct Foo
     x::Int
 end
-
 @test Base.binding_kind(@__MODULE__, :Foo) == Base.PARTITION_KIND_CONST
+
+Base.delete_binding(@__MODULE__, :Foo)
+@test Base.binding_kind(@__MODULE__, :Foo) == Base.PARTITION_KIND_GUARD
+
+const Foo = 1
+@test Base.binding_kind(@__MODULE__, :Foo) == Base.PARTITION_KIND_CONST
+Base.delete_binding(@__MODULE__, :Foo)
 
 # from julia/base/runtime_internals.jl
 # function lookup_binding_partition(world::UInt, b::Core.Binding)
