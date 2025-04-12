3c9849fefd002eeb67423a9eb10b30e656b86b4a
WooKyoung Noh <wookay.noh@gmail.com>
2025-04-12 15:23:18 +0900

    test Base.binding_kind

--- test/compiler/jl_partition_kind.jl
+++ test/compiler/jl_partition_kind.jl
@@ -40,4 +40,24 @@ gr2 = GlobalRef(@__MODULE__, :Foo)
 partition2 = Base.lookup_binding_partition(world2, gr2)
 @test partition2.kind == Base.PARTITION_KIND_CONST
 
+const const_var = 42
+@test Base.binding_kind(@__MODULE__, :const_var) == Base.PARTITION_KIND_CONST
+
+global global_var
+@test Base.binding_kind(@__MODULE__, :global_var) == Base.PARTITION_KIND_DECLARED
+global_var = 1
+@test Base.binding_kind(@__MODULE__, :global_var) == Base.PARTITION_KIND_GLOBAL
+
+@test Base.binding_kind(Base, :pi)        == Base.PARTITION_KIND_EXPLICIT
+@test Base.binding_kind(@__MODULE__, :pi) == Base.PARTITION_KIND_IMPLICIT_CONST
+
+module M
+imported_var = 42
+end
+@test Base.binding_kind(M, :imported_var) == Base.PARTITION_KIND_GLOBAL
+
+@test Base.binding_kind(@__MODULE__, :imported_var) == Base.PARTITION_KIND_GUARD
+import .M: imported_var
+@test Base.binding_kind(@__MODULE__, :imported_var) == Base.PARTITION_KIND_IMPORTED
+
 end # module test_compiler_jl_partition_kind
