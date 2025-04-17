d6e1110d89d534c4bb91656230ffe83d81cbd77a
WooKyoung Noh <wookay.noh@gmail.com>
2025-04-18 08:35:34 +0900

    fix WARNING: Method definition typeinf(Nothing, Core.MethodInstance, UInt8)

--- test/compiler/reflection.jl
+++ test/compiler/reflection.jl
@@ -10,6 +10,12 @@ if VERSION >= v"1.12.0-DEV.1571"
 @eval Main begin
     __custom_compiler_active = true
 end
+
+# julia/Compiler/src/bootstrap.jl
+# WARNING: Method definition typeinf(Nothing, Core.MethodInstance, UInt8) in module Compiler at ../usr/share/julia/Compiler/src/bootstrap.jl:12 overwritten on the same line (check for duplicate calls to `include`).
+using Jive
+Jive.delete(Core.OptimizedGenerics.CompilerPlugins.typeinf, Tuple{Nothing, Core.MethodInstance, UInt8})
+
 using Base: Compiler
 Compiler.activate!(; codegen = true)
 
