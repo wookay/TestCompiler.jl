2680a914595fa69afe8aebbe3262a89bc325dc75
WooKyoung Noh <wookay.noh@gmail.com>
2025-04-26 13:03:24 +0900

    test anonymous function

--- test/compiler/methods.jl
+++ test/compiler/methods.jl
@@ -14,4 +14,13 @@ f() = 42
 method = only(methods(f))
 @test !Compiler.is_declared_inline(method)
 
+using Jive
+Jive.delete(f)
+
+g = (::Int) -> 42
+method = only(methods(g))
+@test method.name === Symbol("#1")
+@test method.sig.types[1] === typeof(g)
+@test method.sig.types[2] === Int
+
 end # module test_compiler_methods
