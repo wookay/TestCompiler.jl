c713d429a7109ac5a64fea054d8d660f53a7ecb3
WooKyoung Noh <wookay.noh@gmail.com>
2025-05-03 12:47:56 +0900

    test Base.datatype_fieldcount

---
+++ test/compiler/runtime_internals.jl
@@ -0,0 +1,12 @@
+module test_compiler_runtime_internals
+
+using Test
+
+@test Base.datatype_fieldcount(Tuple)            == nothing
+@test Base.datatype_fieldcount(Int)              == 0
+@test Base.datatype_fieldcount(typeof(+))        == 0
+@test Base.datatype_fieldcount(Tuple{Int})       == 1
+@test Base.datatype_fieldcount(Vector{Int})      == 2
+@test Base.datatype_fieldcount(NTuple{100, Int}) == 100
+
+end # module test_compiler_runtime_internals
