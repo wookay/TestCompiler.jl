17d78d7fe2c0b6e4a729fd571c76e33d6e6e1f86
WooKyoung Noh <wookay.noh@gmail.com>
2025-05-20 15:05:51 +0900

    manual test cthulhu

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,8 @@
 using Jive
-runtests(@__DIR__)
+targets = split("compiler juliainterpreter pkg testcompiler")
+if false # 18.83 seconds
+    if !haskey(ENV, "CI")
+        VERSION >= v"1.12-beta" && push!(targets, "cthulhu")
+    end
+end
+runtests(@__DIR__, targets=targets)
