c6ce8c27b098475b44f7a3dcb4ad1cd1b7a5473d
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-01 07:56:04 +0900

    test Revise

--- test/compiler/methods.jl
+++ test/compiler/methods.jl
@@ -6,21 +6,21 @@ using Core: Compiler as CC
 # from julia/Compiler/src/utilities.jl
 CC.is_declared_inline
 
-@inline f_inline() = 42
-method = only(methods(f_inline))
+@inline g_inline() = 42
+method = only(methods(g_inline))
 @test CC.is_declared_inline(method)
 
-f() = 42
-method = only(methods(f))
+g() = 42
+method = only(methods(g))
 @test !CC.is_declared_inline(method)
 
 using Jive
-Jive.delete(f)
+Jive.delete(g)
 
-g = (::Int) -> 42
-method = only(methods(g))
+h = (::Int) -> 42
+method = only(methods(h))
 @test first(String(method.name)) == '#'
-@test method.sig.types[1] === typeof(g)
+@test method.sig.types[1] === typeof(h)
 @test method.sig.types[2] === Int
 
 method = only(methods(Base.read, Tuple{String, Type{String}}))
@@ -29,5 +29,22 @@ ast = Base.uncompressed_ast(method)
 @test ast.code[1] === GlobalRef(Base, :open)
 bodyfunc = Base.bodyfunction(method)
 @test bodyfunc === Base.open
+
+
+f(x) = 42
+method = only(methods(f))
+@test Base.specializations(method) == Base.MethodSpecializations(Core.svec())
+
+f(0)
+method = only(methods(f))
+@test Base.specializations(method).specializations ==
+    Base.method_instance(f, Tuple{Int})
+
+f("")
+method = only(methods(f))
+@test Base.specializations(method).specializations == Core.svec(
+    Base.method_instance(f, Tuple{Int}),
+    Base.method_instance(f, Tuple{String}),
+    nothing, nothing, nothing, nothing, nothing)
 
 end # module test_compiler_methods
---
+++ test/revise/track.jl
@@ -0,0 +1,6 @@
+module test_revise_track
+
+using Revise
+Revise.track(Base)
+
+end # module test_revise_track
