74bc40a56e235b6cab3561692d05f3318b6f23fd
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-04 07:24:10 +0900

    test Core.kwcall

--- test/compiler/core.jl
+++ test/compiler/core.jl
@@ -1,3 +1,21 @@
+module test_compiler_core_kwcall
+
+using Test
+
+function g(f, args...; kwargs...)
+    kwargs = merge((;), kwargs)
+    Core.kwcall(kwargs, f, args...)
+end
+
+f(args...; kwargs...) = (args = args, kwargs = kwargs)
+
+@test g(f)           == (args = (),    kwargs = pairs((;)))
+@test g(f   ; b = 2) == (args = (),    kwargs = pairs((; b = 2)))
+@test g(f, 1; b = 2) == (args = (1, ), kwargs = pairs((; b = 2)))
+
+end # module test_compiler_core_kwcall
+
+
 #=
 help?> Core.CodeInfo
   mutable struct Core.CodeInfo
