819ebfa7bfcb743fd17fd8e62b0e4191faba2acc
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-03 15:35:28 +0900

    test Base.invoke_in_world

--- test/compiler/worlds.jl
+++ test/compiler/worlds.jl
@@ -5,4 +5,13 @@ using Test
 # from julia/test/worlds.jl
 @test_throws UndefVarError x
 
+f() = 1
+wc1 = Base.get_world_counter()
+
+f() = 2
+wc2 = Base.get_world_counter()
+
+@test Base.invoke_in_world(wc1, f) == 1
+@test Base.invoke_in_world(wc2, f) == 2
+
 end # module test_compiler_worlds
