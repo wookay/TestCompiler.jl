9d11fde12bdd815460327708698d6cfbc224d358
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-18 23:32:23 +0900

    test extension

--- ext/TestCompilerExt.jl
+++ ext/TestCompilerExt.jl
@@ -1,5 +1,6 @@
 module TestCompilerExt
 
+using TestCompiler
 using Compiler
 
 end # module TestCompilerExt
--- test/testcompiler/ext.jl
+++ test/testcompiler/ext.jl
@@ -1,18 +1,13 @@
 module test_testcompiler_ext
 
 using Test
-
 using Core: Compiler as CC
-@test !Core.isdefined(CC, :hello_ext)
-
 using Compiler
-# Compiler.activate!(; codegen = true)
 
 @test Compiler.BaseCompiler === CC
 @test Compiler.BaseCompiler !== Compiler
 
 using TestCompiler
-
 ext = Base.get_extension(TestCompiler, :TestCompilerExt)
 @test ext isa Module
 
