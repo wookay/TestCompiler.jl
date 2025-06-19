f632335261094f227dd070b178b56ec13ac9a169
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-19 09:08:25 +0900

    deps Compiler

--- test/revise/track.jl
+++ test/revise/track.jl
@@ -1,6 +1,11 @@
 module test_revise_track
 
+# don't test Revise on CI
+
 using Revise
 Revise.track(Base)
+
+using Test
+@test true
 
 end # module test_revise_track
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using Jive
 targets = split("corecompiler juliainterpreter pkg testcompiler")
-if false # 18.83 seconds
-    if !haskey(ENV, "CI")
-        VERSION >= v"1.12-beta" && push!(targets, "cthulhu")
-    end
+on_ci = haskey(ENV, "CI")
+if false # takes 18.83 seconds
+    !on_ci && VERSION >= v"1.12-beta" && push!(targets, "cthulhu")
+end
+if false # takes long
+    !on_ci && push!(targets, "revise")
 end
 runtests(@__DIR__, targets=targets)
