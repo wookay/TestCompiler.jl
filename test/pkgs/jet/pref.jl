893a7fd3b0c80ebd08d3138d8e4498ba796e9efc
WooKyoung Noh <wookay.noh@gmail.com>
2025-06-21 00:15:34 +0900

    test JET

---
+++ test/pkgs/jet/pref.jl
@@ -0,0 +1,4 @@
+# https://github.com/aviatesk/JET.jl
+
+using Preferences
+Preferences.set_preferences!("JET", "JET_DEV_MODE" => true)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using Jive
-targets = split("corecompiler juliainterpreter pkg testcompiler")
+targets = split("corecompiler testcompiler pkgs/juliainterpreter")
 on_ci = haskey(ENV, "CI")
 if false # takes 18.83 seconds
-    !on_ci && VERSION >= v"1.12-beta" && push!(targets, "cthulhu")
+    !on_ci && VERSION >= v"1.12-beta" && push!(targets, "pkgs/cthulhu")
 end
 if false # takes long
-    !on_ci && push!(targets, "revise")
+    !on_ci && push!(targets, "pkgs/revise")
 end
 runtests(@__DIR__, targets=targets)
