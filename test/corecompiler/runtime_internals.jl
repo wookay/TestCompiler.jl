6d543dba7b09952bb8ee194d68f526669e1aa9ba
WooKyoung Noh <wookay.noh@gmail.com>
2025-07-19 10:38:20 +0900

    test `@__FUNCTION__` macro

--- test/corecompiler/runtime_internals.jl
+++ test/corecompiler/runtime_internals.jl
@@ -10,3 +10,18 @@ using Test
 @test Base.datatype_fieldcount(NTuple{100, Int}) == 100
 
 end # module test_corecompiler_runtime_internals
+
+
+using Jive
+# @__FUNCTION__  julia commit bea90a2f503018115b30559b9d6838c05a86b9b4
+@If VERSION >= v"1.13.0-DEV.878" module test_corecompiler_runtime_internals__function_macro
+
+using Test
+
+function f()
+    @__FUNCTION__
+end
+
+@test f() === f
+
+end # module test_corecompiler_runtime_internals__function_macro
