354650d6dde59e724f6cb3ff5372bd04a0cb4db1
WooKyoung Noh <wookay.noh@gmail.com>
2025-07-19 10:49:15 +0900

    test `Expr(:thisfunction)`

--- test/corecompiler/runtime_internals.jl
+++ test/corecompiler/runtime_internals.jl
@@ -14,14 +14,18 @@ end # module test_corecompiler_runtime_internals
 
 using Jive
 # @__FUNCTION__  julia commit bea90a2f503018115b30559b9d6838c05a86b9b4
-@If VERSION >= v"1.13.0-DEV.878" module test_corecompiler_runtime_internals__function_macro
+@If VERSION >= v"1.13.0-DEV.878" module test_corecompiler_runtime_internals_function_macro
 
 using Test
 
-function f()
-    @__FUNCTION__
+function g()
+    f = @__FUNCTION__
+    nameof(f)
 end
 
-@test f() === f
+@test g() === :g
 
-end # module test_corecompiler_runtime_internals__function_macro
+λ() = eval(Expr(:thisfunction))
+@test λ isa Function
+
+end # module test_corecompiler_runtime_internals_function_macro
