7521ed72a3d4b41a828f32c1c4afc573eeaecd9a
WooKyoung Noh <wookay.noh@gmail.com>
2025-07-20 18:28:49 +0900

    test Base.acquire Base.release, Base.@acquire

---
+++ test/corecompiler/misc.jl
@@ -0,0 +1,16 @@
+module test_corecompiler_misc_semaphore
+
+using Test
+
+if VERSION >= v"1.13"
+# from julia/test/misc.jl
+sem_size = 2
+s = Base.Semaphore(sem_size)
+Base.acquire(s)
+1+2
+Base.release(s)
+
+Base.@acquire s 1+2
+end
+
+end # module test_corecompiler_misc_semaphore
