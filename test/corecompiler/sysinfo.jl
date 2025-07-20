406413a8959f906ddb0fc0b03fca3ed0e3998d41
WooKyoung Noh <wookay.noh@gmail.com>
2025-07-20 18:35:12 +0900

    test Sys.sysimage_target()

--- test/corecompiler/sysinfo.jl
+++ test/corecompiler/sysinfo.jl
@@ -3,8 +3,13 @@ module test_corecompiler_sysinfo
 using Test
 
 if VERSION >= v"1.13.0-DEV.877" # julia commit 3b2918785597f9b7b05f7b3e479ea89716ca22bd
-@info Sys.sysimage_target()
-# @test !haskey(ENV, "JULIA_CPU_TARGET") && Sys.sysimage_target() == "sysimage"
+    # Sys.sysimage_target() == "sysimage"
+    if haskey(ENV, "CI")
+        @test Sys.sysimage_target() == "native" # github actions
+    end
+    if haskey(ENV, "JULIA_CPU_TARGET")
+        @test ENV["JULIA_CPU_TARGET"] == Sys.sysimage_target()
+    end
 end
 
 end # module test_corecompiler_sysinfo
