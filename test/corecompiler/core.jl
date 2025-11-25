94cbb3a0e30a15563259e60dbeabd81b80ea915e
WooKyoung Noh <wookay.noh@gmail.com>
2025-11-26 07:08:58 +0900

    test `finalizer`, `finalize`

--- test/corecompiler/core.jl
+++ test/corecompiler/core.jl
@@ -16,6 +16,33 @@ f(args...; kwargs...) = (args = args, kwargs = kwargs)
 end # module test_corecompiler_core_kwcall
 
 
+module test_corecompiler_core_finalizers
+
+using Test
+
+# from julia/test/core.jl
+
+let A = [9], x = 0
+    @test ismutable(A)
+    finalizer(_->(x+=1), A)
+    @test x == 0
+    finalize(A)
+    @test x == 1
+end
+
+let A = [9], x = 0
+    finalizer(_->(x+=1), A)
+
+    A = []
+    finalize(A)
+    @test x == 0
+end
+
+@test !isdefined(@__MODULE__, :A)
+
+end # module test_corecompiler_core_finalizers
+
+
 #=
 help?> Core.CodeInfo
   mutable struct Core.CodeInfo
