dd2a49e566b8566029dd8a6fcf84cdb0736cb9c0
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-25 21:30:38 +0900

    check `DEPOT_PATH[1]` for `Base.compilecache_dir`

--- test/compiler/precompile.jl
+++ test/compiler/precompile.jl
@@ -5,7 +5,9 @@ using Test
 
 pkgid = Base.PkgId(Base.UUID("807dbc54-b67e-4c79-8afb-eafe4df6f2e1"), "Compiler")
 loaded = haskey(Base.loaded_modules, pkgid)
+if isdir(DEPOT_PATH[1])
 @test isdir(Base.compilecache_dir(pkgid))
+end
 
 using Compiler
 loaded = haskey(Base.loaded_modules, pkgid)
