a43e5faa087f18519747a055d86e37422928e009
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-31 10:26:05 +0900

    test φ, φᶜ, ϒ

--- test/corecompiler/ast.jl
+++ test/corecompiler/ast.jl
@@ -16,8 +16,8 @@ using .CC: NewvarNode, CodeInfo, LineNumberNode, GotoNode, GotoIfNot, EnterNode,
 @test repr(ReturnNode()) == ":(unreachable)"
 @test repr(GotoIfNot(true, 4)) == ":(goto %4 if not true)"
 @test repr(PhiNode(Int32[2, 3], Any[1, SlotNumber(3)])) == ":(φ (%2 => 1, %3 => _3))"
-@test repr(UpsilonNode(SlotNumber(3))) == ":(ϒ (_3))"
 @test repr(PhiCNode(Any[1, SlotNumber(3)])) == ":(φᶜ (1, _3))"
+@test repr(UpsilonNode(SlotNumber(3))) == ":(ϒ (_3))"
 @test sprint(Base.show_unquoted, Argument(23)) == "_23"
 @test sprint(Base.show_unquoted, Argument(-2)) == "_-2"
 
@@ -27,8 +27,18 @@ Base.isa_ast_node
 Base.is_self_quoting
 end
 
-# from julia/base/coreir.jl
-@test PhiNode() == PhiNode(Int32[], Any[])
+φ = PhiNode()
+@test φ.edges == Int32[]
+@test φ.values == Any[]
+@test φ == PhiNode(Int32[], Any[])
+
+φᶜ = PhiCNode([])
+@test isempty(φᶜ.values)
+
+ϒ = UpsilonNode()
+@test hasfield(typeof(ϒ), :val)
+@test !isdefined(ϒ, :val)
+
 
 # ExprNode from julia/base/show.jl
 using .CC: Expr, QuoteNode, SlotNumber, LineNumberNode, SSAValue,
