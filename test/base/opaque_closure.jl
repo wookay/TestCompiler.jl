9b96ffe4c59da4819a1c825b51c7eb771354b87c
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-08 23:28:36 +0900

    test code_warntype

--- test/base/opaque_closure.jl
+++ test/base/opaque_closure.jl
@@ -1,6 +1,7 @@
 module test_base_opaque_closure
 
 using Test
+using InteractiveUtils: code_warntype
 using Base.Experimental: @opaque
 
 oc = @opaque(x->2x)
@@ -10,6 +11,33 @@ oc = @opaque(x->2x)
 @test oc.source isa Core.Method
 @test oc.invoke isa Ptr{Nothing}
 @test oc.specptr isa Ptr{Nothing}
+
+# from julia/test/opaque_closure.jl
+let oc = @opaque (a::Int) -> identity(a)
+    buf = IOBuffer()
+    code_warntype(buf, oc, (Int,); optimize=true)
+    opt = String(take!(buf))
+#=
+Arguments
+  #self#::Core.Const(())
+  a::Int64
+Body::Int64
+1 ─     return a
+=#
+
+    code_warntype(buf, oc, (Int,); optimize=false)
+    unopt = String(take!(buf))
+#=
+Arguments
+  #self#
+  a
+Body::Int64
+1 ─ %1 = Main.test_base_opaque_closure.identity
+│   %2 = (%1)(a)
+└──      return %2
+=#
+    @test opt != unopt
+end
 
 # from julia/base/opaque_closure.jl
 #=
