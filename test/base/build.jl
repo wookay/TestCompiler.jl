0fb80dcadff0f5adb3c48eae15d2cabfbe58c436
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-22 08:25:23 +0900

    test sysimg

--- test/base/build.jl
+++ test/base/build.jl
@@ -4,7 +4,11 @@ using Test
 
 # from julia/contrib/generate_precompile.jl
 sysimg = Base.unsafe_string(Base.JLOptions().image_file)
+if Sys.isapple()
 @test endswith(sysimg, #= usr =# "/lib/julia/sys.dylib")
+else
+@test endswith(sysimg, #= /opt/hostedtoolcache/julia/1.11.8/x64 =# "/lib/julia/sys.so")
+end
 
 if VERSION >= v"1.12"
 @test Base.__precompile_script.debug_output === devnull
