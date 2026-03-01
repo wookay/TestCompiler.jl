b9911621788d35fa7db26be69b594e8aa2708cb3
WooKyoung Noh <wookay.noh@gmail.com>
2026-03-01 15:46:15 +0900

    test compiler inference

---
+++ test/compiler/inference.jl
@@ -0,0 +1,19 @@
+using Jive
+# julia commit 44c835795b
+@If VERSION >= v"1.14.0-DEV.1826" module test_compiler_inference
+
+using Test
+
+# from julia/Compiler/test/inference.jl
+function f(x::Any)
+    y = x
+    if x isa Int
+        return sin(y)
+    end
+    error("x is not Int")
+end
+@test Base.infer_return_type(f, (Any,)) == Float64
+@test Base.infer_return_type(f, (Int,)) == Float64
+@test Base.infer_return_type(f, (String,)) == Union{}
+
+end # module test_compiler_inference
