feecda52f0b80c7634c0ec63f01d6c076502af7e
WooKyoung Noh <wookay.noh@gmail.com>
2026-03-21 07:37:37 +0900

    test return type with side effects

--- test/base/worlds.jl
+++ test/base/worlds.jl
@@ -15,4 +15,12 @@ wc2::UInt64 = Base.get_world_counter()
 @test Base.invoke_in_world(wc1, f) == 1
 @test Base.invoke_in_world(wc2, f) == 2
 
+
+# from julia/test/worlds.jl
+rettype_with_side_effect() = eval(:(rettype_side_effect = "blah"; Cint))
+if VERSION >= v"1.14.0-DEV.1921" # julia commit d79443c60e
+    @test rettype_side_effect == "blah"
+    ccall(:strlen, rettype_with_side_effect(), (Cstring,), "xx")
+end
+
 end # module test_base_worlds
