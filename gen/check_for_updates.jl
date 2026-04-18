85ed60687f3b33d97af58cb2a1b44e57a00e953f
WooKyoung Noh <wookay.noh@gmail.com>
2026-04-18 13:17:55 +0900

    add a macro `@check_for_updates`

--- gen/check_for_updates.jl
+++ gen/check_for_updates.jl
@@ -9,30 +9,41 @@ const emojis = Dict(
     :TestCompiler => "🚗",
 )
 
-function check_for_updates(modules...)
+function get_target_modules(modules::Vector{Any})
     if isempty(ARGS)
         target_modules = modules
     else
         target_pkgs = Symbol.(ARGS)
-        module_syms = collect(nameof.(modules))
-        inds = indexin(target_pkgs, module_syms)
+        inds = indexin(target_pkgs, modules)
         if all(isnothing, inds)
-            throw(ArgumentError(repr(ARGS)))
+            throw(ArgumentError(string("ARGS = ", ARGS)))
             return
         else
             target_modules = getindex(modules, inds)
         end
     end
+    return target_modules
+end
 
+function check_for_updates(mod::Module)
+    printstyled(stdout, "### ", emojis[nameof(mod)], " ", mod, "\n"; color = :yellow)
+    pkg_filepath = pathof(mod)
+    script_path = normpath(pkg_filepath, "../../gen/check_for_updates_using_sugar_cubes.jl")
+    @test isfile(script_path)
     julia_cmd = Base.julia_cmd()
+    args = ["--color=yes", script_path]
+    cmd = Cmd([julia_cmd..., args...])
+    @test success(run(cmd))
+end
+
+macro check_for_updates(expr::Expr)
+    modules = expr.args
+    target_modules = get_target_modules(modules)
     for mod in target_modules
-        printstyled(stdout, "### ", emojis[nameof(mod)], " ", mod, "\n"; color = :yellow)
-        pkg_filepath = pathof(mod)
-        script_path = normpath(pkg_filepath, "../../gen/check_for_updates_using_sugar_cubes.jl")
-        @test isfile(script_path)
-        args = ["--color=yes", script_path]
-        cmd = Cmd([julia_cmd..., args...])
-        @test success(run(cmd))
+        @eval begin
+            using $mod
+            check_for_updates($mod)
+        end
     end
 end
 
@@ -41,5 +52,4 @@ using             TestCompiler
 check_for_updates(TestCompiler)
 end
 
-using             FemtoCompiler, Jive, TestJiveRunMoreTests, EmojiSymbols, DumpTruck
-check_for_updates(FemtoCompiler, Jive, TestJiveRunMoreTests, EmojiSymbols, DumpTruck)
+@check_for_updates FemtoCompiler, Jive, TestJiveRunMoreTests, EmojiSymbols, DumpTruck
