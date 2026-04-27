95441388052a3e3cb25c07b2f3a98a98a767e958
WooKyoung Noh <wookay.noh@gmail.com>
2026-04-27 22:44:38 +0900

    test `Base.Experimental` hint handlers

--- test/base/experimental.jl
+++ test/base/experimental.jl
@@ -1,6 +1,6 @@
-# see also  opaque_closure.jl
-
 using Jive
+
+# see also  opaque_closure.jl
 @If VERSION >= v"1.14.0-DEV.16" module test_base_experimental_reexport
 
 using Test
@@ -19,3 +19,36 @@ using .M2
 @test f1 isa Function
 
 end # module test_base_experimental_reexport
+
+
+@If VERSION >= v"1.12.0-DEV.91" module test_base_experimental_hint_handlers
+
+using Test
+using REPL
+
+function get_hint_handlers(T::Type{<: Exception})
+    if VERSION >= v"1.13.0-DEV.1358"
+        [f for (Err, f) in Base.Experimental._hint_handlers[Core.typename(T)]]
+    else
+        Base.Experimental._hint_handlers[T]
+    end
+end
+
+@test get_hint_handlers(UndefVarError) == [
+        Base.UndefVarError_hint,
+        REPL.UndefVarError_REPL_hint
+    ]
+
+@test get_hint_handlers(MethodError) == [
+        Base.noncallable_number_hint_handler,
+        Base.nonsetable_type_hint_handler,
+        Base.string_concatenation_hint_handler,
+        Base.methods_on_iterable
+    ]
+
+@test get_hint_handlers(FieldError) == [
+        Base.fielderror_dict_hint_handler,
+        Base.fielderror_listfields_hint_handler
+    ]
+
+end # module test_base_experimental_hint_handlers
