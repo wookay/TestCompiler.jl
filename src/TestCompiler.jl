47e82de2f0afcda0209b17e7a7bd5d6c5607bfe3
WooKyoung Noh <wookay.noh@gmail.com>
2026-04-30 05:30:14 +0900

    remove list of files in SysImage

--- src/TestCompiler.jl
+++ src/TestCompiler.jl
@@ -5,7 +5,6 @@ include("PartitionKinds.jl")
 include("EffectBits/EffectBits.jl")
 include("extension-interface.jl")
 include("TraceEvents.jl")
-include("SysImage.jl")
 Base.generating_output() && include("precompile.jl")
 
 end # module TestCompiler
--- test/testcompiler/sysimage.jl
+++ test/testcompiler/sysimage.jl
@@ -1,6 +1,7 @@
 module test_testcompiler_sysimage
 
 using Test
+#=
 using TestCompiler: SysImage
 
 @test "base/Base_compiler.jl" in SysImage.COMPILER_SRCS
@@ -8,5 +9,6 @@ using TestCompiler: SysImage
 @test "base/Base.jl" in SysImage.BASE_SRCS
 @test "base/sysimg.jl" in SysImage.STDLIB_SRCS
 @test "usr/share/julia/JuliaLowering/src/desugaring.jl" in SysImage.JULIALOWERING_SRCS
+=#
 
 end # module test_testcompiler_sysimage
