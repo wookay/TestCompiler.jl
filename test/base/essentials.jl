0bd2c236d5aeaa023050adb758eb782f6dfccb4a
WooKyoung Noh <wookay.noh@gmail.com>
2026-05-09 07:13:54 +0900

    test `Core.PartialStruct` (2)

--- test/base/essentials.jl
+++ test/base/essentials.jl
@@ -25,18 +25,27 @@ const ACat = AbstractCat{T} where T
 
 # from julia/Compiler/test/inference.jl
 using Core: PartialStruct, Compiler as CC
-using .CC: Const
-typ = Tuple{Int64, UInt64}
 if VERSION >= v"1.12"
+using .CC: Const, Future, AbstractIterationResult
+typ = Tuple{Int64, UInt64}
 PT = PartialStruct(CC.fallback_lattice, typ, Any[Const(10), UInt64])
 @test isa(PT, PartialStruct)
 @test PT.typ === typ
 # julia commit 199855161e
 # from julia/Compiler/src/abstractinterpretation.jl
-# function precise_container_type
+# `typ` is the inferred type for expression `arg`.
+# if the expression constructs a container (e.g. `svec(x,y,z)`),
+# refine its type to an array of element types.
+# Union of Tuples of the same length is converted to Tuple of Unions.
+# returns an array of types
+# function precise_container_type(interp::AbstractInterpreter, @nospecialize(itft), @nospecialize(typ),
+#                                 vtypes::Union{VarTable,Nothing}, sv::AbsIntState)
 widet = Base.unwrap_unionall(PT.typ)
 @test isa(widet, DataType)
 @test widet === typ
+@test widet.name === Tuple.name
+future = Future(AbstractIterationResult(PT.fields, nothing))
+@test future isa Future
 end # if
 
 end # module test_base_essentials
