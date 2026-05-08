5b83cc2e17a1311b655f4c02f9a481b0da23fb3b
WooKyoung Noh <wookay.noh@gmail.com>
2026-05-08 14:33:35 +0900

    test `Core.PartialStruct`

--- test/base/essentials.jl
+++ test/base/essentials.jl
@@ -23,4 +23,20 @@ const ACat = AbstractCat{T} where T
 @test ACat isa UnionAll
 @test Base.unwrap_unionall(ACat) === ACat.body
 
+# from julia/Compiler/test/inference.jl
+using Core: PartialStruct, Compiler as CC
+using .CC: Const
+typ = Tuple{Int64, UInt64}
+if VERSION >= v"1.12"
+PT = PartialStruct(CC.fallback_lattice, typ, Any[Const(10), UInt64])
+@test isa(PT, PartialStruct)
+@test PT.typ === typ
+# julia commit 199855161e
+# from julia/Compiler/src/abstractinterpretation.jl
+# function precise_container_type
+widet = Base.unwrap_unionall(PT.typ)
+@test isa(widet, DataType)
+@test widet === typ
+end # if
+
 end # module test_base_essentials
