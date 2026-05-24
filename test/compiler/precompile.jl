1546cf7f52077016752038f220e6ef62c94ae5f2
WooKyoung Noh <wookay.noh@gmail.com>
2026-05-24 14:01:54 +0900

    update test `Base.compilecache_dir`

--- test/compiler/precompile.jl
+++ test/compiler/precompile.jl
@@ -2,12 +2,13 @@ using Jive
 @If VERSION >= v"1.14-DEV" module test_compiler_precompile
 
 using Test
+using Pkg
 
 pkgid = Base.PkgId(Base.UUID("807dbc54-b67e-4c79-8afb-eafe4df6f2e1"), "Compiler")
 loaded = haskey(Base.loaded_modules, pkgid)
-if isdir(DEPOT_PATH[1])
-@test isdir(Base.compilecache_dir(pkgid))
-end
+@test Pkg.depots1() == DEPOT_PATH[1] # $HOME/.julia
+ver = string("v", VERSION.major, ".", VERSION.minor)
+@test joinpath(Pkg.depots1(), "compiled", ver, "Compiler") == Base.compilecache_dir(pkgid) # $HOME/.julia/compiled/v1.14/Compiler
 
 using Compiler
 loaded = haskey(Base.loaded_modules, pkgid)
