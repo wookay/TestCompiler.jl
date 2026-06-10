2f938a8c5926378e589bd5aebbbbd9dbdd6579b1
WooKyoung Noh <wookay.noh@gmail.com>
2026-06-10 10:41:06 +0900

    test Core.DebugInfo

--- test/testcompiler/lookup_method_instance.jl
+++ test/testcompiler/lookup_method_instance.jl
@@ -10,4 +10,12 @@ do_work(x, y) = x + y
 mi = lookup_method_instance(do_work, 1, 2)
 @test mi isa Core.MethodInstance
 
+# from julia/Compiler/src/ssair/ir.jl
+#      julia/Compiler/test/ssair.jl
+di = Core.DebugInfo(mi)
+@test di.def === mi
+@test di.linetable === nothing
+@test di.edges == Core.svec()
+@test di.codelocs == ""
+
 end # using test_CompilerDevTools_lookup_method_instance
