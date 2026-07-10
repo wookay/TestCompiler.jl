d16e561252c1ee7038f21a0371da73acb394ac35
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-10 15:55:49 +0900

    test wrapping arithmetic op

--- test/base/operators.jl
+++ test/base/operators.jl
@@ -7,3 +7,14 @@ using Test
 @test (≠) === (!=)
 
 end # module test_base_operators
+
+
+using Jive
+@If VERSION >= v"1.14.0-DEV.2629" module test_base_operators_wrapping_arithmetic_op
+# julia commit b6e5cb5a65
+
+using Test
+op = -%
+@test op(3, 2) == 1
+
+end # module test_base_operators_wrapping_arithmetic_op
