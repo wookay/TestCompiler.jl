dc6cd6714bcb45b1fd2ca38451605f1f8a4a55bf
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-23 07:36:48 +0900

    test `Base.invoke_in_world` kwcall

--- test/base/worlds.jl
+++ test/base/worlds.jl
@@ -29,3 +29,30 @@ if VERSION >= v"1.14.0-DEV.1921" || # julia commit d79443c60e
 end
 
 end # module test_base_worlds
+
+
+module test_base_worlds_warn_overwrite
+
+using Test
+
+f(; a = 1) = a
+
+wc1::UInt64 = Base.get_world_counter()
+
+# --warn-overwrite=yes
+# WARNING: Method definition f() in module test_base_worlds_warn_overwrite at worlds.jl:38 overwritten at worlds.jl:43.
+# WARNING: Method definition kwcall(NamedTuple{names, T} where T<:Tuple where names, typeof(test_base_worlds_warn_overwrite.f)) in module test_base_worlds_warn_overwrite at worlds.jl:38 overwritten at worlds.jl:43.
+f(; b = 2) = b
+
+wc2::UInt64 = Base.get_world_counter()
+
+@test_throws MethodError f(; a = 10)
+@test f(; b = 20) == 20
+@test f() == 2
+
+@test Base.invoke_in_world(wc1, f) == 1
+@test Base.invoke_in_world(wc1, f; a = 10) == 10
+@test Base.invoke_in_world(wc2, f) == 2
+@test Base.invoke_in_world(wc2, f; b = 20) == 20
+
+end # module test_base_worlds_warn_overwrite
