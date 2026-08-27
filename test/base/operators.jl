12569dd2a9d3a7d1a98bd325cd030db012f13713
WooKyoung Noh <wookay.noh@gmail.com>
2026-08-28 07:57:53 +0900

    splat ∘ unsplat

--- test/base/operators.jl
+++ test/base/operators.jl
@@ -17,4 +17,13 @@ using Test
 op = -%
 @test op(3, 2) == 1
 
+@test splat(+)((1, 2, 3)) == +(1, 2, 3) == 6
+
+if VERSION >= v"1.14.0-DEV.3057" # julia commit 8c9c94f563
+unsplat
+@test unsplat(sum) === sum ∘ tuple
+@test unsplat(sum)(1, 2, 3) == sum((1, 2, 3)) == 6
+@test (splat ∘ unsplat)(sum) === (unsplat ∘ splat)(sum) === sum
+end
+
 end # module test_base_operators_wrapping_arithmetic_op
